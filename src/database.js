0867d5099e75849c24498086bad7e45b8496377b
Diego Alcantara <diegoalcantara2003@gmail.com>
2024-08-27 16:05:11 -0300

    Add file database.js
    Adicionado o arquivo database.js para estabelecer a conexão com o banco de dados

---
+++ src/database.js
@@ -0,0 +1,19 @@
+// Configurações de conexão ao banco de dados
+
+require('dotenv').config({path: 'variaveis.env'})
+
+// importação do módulo MYSQL
+const mysql = require('mysql2')
+// criar a conexão
+const connection = mysql.createConnection({
+    host: process.env.BD_HOST,
+    user: process.env.BD_USER,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_NAME
+})
+
+// conectar a conexão
+connection.connect(err => {
+    if (err) throw err
+    console.log(`connection to database ${process.env.BD_NAME}`)
+});
